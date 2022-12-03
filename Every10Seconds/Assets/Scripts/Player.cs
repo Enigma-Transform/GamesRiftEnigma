c48474027e7f257ed005b82634313462aa03690c
Enigma-Transform <75926695+Enigma-Transform@users.noreply.github.com>
2022-12-03 23:24:11 +0000

    Commit1

---
+++ Every10Seconds/Assets/Scripts/Player.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody rb;
+
+    [SerializeField]
+    bool isKeyPressedD,isKeyPressedA,isKeyPressed;
+
+    [SerializeField]
+    Vector3 torque;
+    [Range(0,100)]
+    [SerializeField]
+    float rotSpeed;
+
+    [Range(0, 100)]
+    [SerializeField]
+    float speed;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    private void Update()
+    { 
+    }
+        // Update is called once per frame
+    void FixedUpdate()
+    {
+       float direction = Input.GetAxisRaw("Vertical");
+        float turn = Input.GetAxisRaw("Horizontal");
+        Vector3 rot = new Vector3(0, 10, 0);
+
+
+        // transform.Rotate(0, transform.position.y * rotSpeed, 0,Space.Self);
+        //rb.MoveRotation(Quaternion.Euler(rot*speed));
+        if (turn > 0)
+        {
+            rb.AddTorque(transform.up* turn * rotSpeed);
+
+        }
+        else if (turn < 0)
+        {
+            rb.AddTorque(-transform.up * -turn * rotSpeed);
+
+        }
+
+
+        if (direction > 0)
+        {
+            rb.velocity = transform.forward * speed;
+        }
+        else if (direction < 0)
+        {
+            rb.velocity = -transform.forward* speed;
+        }
+
+    }
+
+    
+}
